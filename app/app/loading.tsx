1167da67acdeb4420f8514bf2dde0acc2bcf4544
y4gg <contact@y4.gg>
2026-03-01 11:30:37 +0100

    Add loading screen for home page

---
+++ app/app/loading.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { usePageTitle } from "@/app/app/layout"
+import { Spinner } from "@/components/ui/spinner"
+
+export default function AppLoading() {
+  usePageTitle("Home")
+  return (
+    <div className="flex flex-1 items-center justify-center -mt-16">
+      <Spinner className="size-12" />
+    </div>
+  )
+}
