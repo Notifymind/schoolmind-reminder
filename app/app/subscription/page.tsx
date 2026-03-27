c26e91a33940f1b16aaa1bcd2082066a146dcc00
y4gg <contact@y4.gg>
2026-03-27 23:07:59 +0100

    fix: update notification limit label to be more descriptive

--- app/app/subscription/page.tsx
+++ app/app/subscription/page.tsx
@@ -24,7 +24,7 @@ const features = [
   { name: "Exam Access", free: true, pro: true },
   { name: "Assignment Access", free: false, pro: true },
   { name: "App Notifications", free: true, pro: true },
-  { name: "Max. notifications", free: "2", pro: "5" },
+  { name: "Max. notifications per Preset", free: "2", pro: "7" },
   { name: "Notification Presets", free: "1", pro: "5" },
   { name: "Price/Month", free: "Free", pro: "4KM" },
   { name: "Price/Year:", free: "Free", pro: "32KM" },
